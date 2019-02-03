786f40dd0f9239a0eea979cd65cea888b3492834
Shelby Inloes <shelbyinloes@gmail.com>
2019-02-02 20:14:09 -0600

    adding in grid layout + header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import Header from './components/Header.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <Container fluid>
+        <Row>
+          <Col sm={12}>
+            <Header />
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
