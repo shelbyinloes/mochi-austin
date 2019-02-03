9f50a995a7d84751f127b8fdfd14b4c4f663456a
Shelby Inloes <shelbyinloes@gmail.com>
2019-02-02 20:46:35 -0600

    carousel

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import Header from './components/Header.js';
+import MochiCarousel from './components/MochiCarousel.js';
 
 class App extends Component {
   render() {
@@ -10,6 +11,9 @@ class App extends Component {
         <Row>
           <Col sm={12}>
             <Header />
+          </Col>
+          <Col sm={12}>
+            <MochiCarousel />
           </Col>
         </Row>
       </Container>
