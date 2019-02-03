411018f8970a697418c43a63e3c8c2fa9f372f6e
Shelby Inloes <shelbyinloes@gmail.com>
2019-02-02 20:16:43 -0600

    components header + carousel

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import headerLogo from '../images/logos/headerLogo.png';
+import { Container, Row, Col } from 'react-bootstrap';
+import './../App.css'
+
+
+const Header = () => (
+  <Container id="header" fluid>
+    <Row>
+      <Col sm={5}>
+        <img src={headerLogo} alt={"Mochi Austin Logo"} id="headerImg"/>
+      </Col>
+      <Col sm={6} id="headerNav">
+        <span>Home</span>
+        <span>Menu</span>
+        <span>About</span>
+        <span>Location</span>
+        <span>Contact</span>
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default Header;
