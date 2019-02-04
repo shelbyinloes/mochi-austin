e42efb538689fb0465cd254731d12ece227d037a
Shelby Inloes <shelbyinloes@gmail.com>
2019-02-03 18:16:16 -0600

    updating carousel

--- src/components/MochiCarousel.js
+++ src/components/MochiCarousel.js
@@ -1,32 +1,48 @@
 import React from 'react';
-import { Container, Carousel } from 'react-bootstrap';
-import mochi from '../images/mochi.jpg';
-import mochi1 from '../images/mochi1.jpg';
-import mochi2 from '../images/mochi2.jpg';
 import './../App.css'
+import { Container, Carousel } from 'react-bootstrap';
+import mochi2 from '../images/mochi2.jpg';
+import mochi3 from '../images/mochi3.jpg';
+import mochi4 from '../images/mochi4.jpg';
+import mochi5 from '../images/mochi5.jpg';
+import mochi6 from '../images/mochi6.jpg';
 
 
 const MochiCarousel = () => (
   <Container id="carousel" fluid>
     <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={mochi}
-          alt="Mochi slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={mochi1}
-          alt="Mochi slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
+      <Carousel.Item className="mochiImg">
         <img
           className="d-block w-100"
           src={mochi2}
+          alt="Mochi slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item className="mochiImg">
+        <img
+          className="d-block w-100"
+          src={mochi3}
+          alt="Mochi slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item className="mochiImg">
+        <img
+          className="d-block w-100"
+          src={mochi4}
+          alt="Mochi slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item className="mochiImg">
+        <img
+          className="d-block w-100"
+          src={mochi5}
+          alt="Mochi slide"
+        />
+      </Carousel.Item>      
+      <Carousel.Item className="mochiImg">
+        <img
+          className="d-block w-100"
+          src={mochi6}
           alt="Mochi slide"
         />
       </Carousel.Item>
