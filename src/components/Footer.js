e758905129aba7544ff8f0cda386bb173c391946
Shelby Inloes <shelbyinloes@gmail.com>
2019-02-07 13:09:12 -0600

    footer styling + adding social btns

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import verticalLogo from '../images/logos/verticallogo.png';
+import mochiFacebook from '../images/mochiFacebook.png';
+import mochiInsta from '../images/mochiInsta.png';
+import mochiPinterest from '../images/mochiPinterest.png';
+import mochiTwitter from '../images/mochiTwitter.png';
 import { Container, Row, Col } from 'react-bootstrap';
 import './../App.css'
 
@@ -11,21 +15,24 @@ const Footer = () => (
         <img src={verticalLogo} alt={"Mochi Austin Logo"} id="footerImg"/>
       </Col>
       <Col sm={3} id="footerAddress">
-        <span>Austin, Texas</span>
+        <span style={{fontSize: '20px'}}>Austin, Texas</span>
         <span>213 Ice Cream Ln</span>
         <span>Austin, TX 78701</span>
         <span>Get Directions</span>
         <span>(512) 555-1122</span>
       </Col>
       <Col sm={3} id="footerHours">
-        <span>Hours:</span>
+        <span style={{fontSize: '20px'}}>Hours:</span>
         <span>Monday - Thursday:</span>
         <span>11am - 9pm</span>
         <span>Friday - Sunday:</span>
         <span>10am - 10pm</span>
       </Col>
       <Col sm={3} id="footerSocial">
-        <h1>Icons</h1>
+        <img src={mochiFacebook} alt={"Facebook"} className="socialImg"/>
+        <img src={mochiInsta} alt={"Instagram"} className="socialImg"/>
+        <img src={mochiPinterest} alt={"Pinterest"} className="socialImg"/>
+        <img src={mochiTwitter} alt={"Twitter"} className="socialImg"/>
       </Col>
       <Col sm={12} id="footerCopy">
         Shelby Inloes, 2019 - Design based off of POKE AUSTIN website
